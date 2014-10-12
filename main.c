f75f315c552f003c9855264ea1f5bec580a47522
mkonstantinou <matt.konstantinou@gmail.com>
2014-10-12 19:04:27 -0400

    fixed duplicate number guess but caused other issues

--- main.c
+++ main.c
@@ -29,6 +29,18 @@ int* checkGuess(char*);
  */
 char* solve(char*);
 
+/* indexOfNumber
+ * pre: takes string and integer
+ * post: returns index of int character in string
+ */
+int indexOfNumber(char*, int);
+
+/* findNegativeOne
+ * pre: takes pointer to list of integers and length of list
+ * post: returns index of first -1 and replaces it with -2
+ */
+int findNegativeOne(int[], int);
+
 int guessCount;
 
 int main(int argc, char** argv)
@@ -88,13 +100,13 @@ int* checkGuess(char* guess)
         }
     }
 
-    returnArray = malloc(2 * sizeof(int));
+    returnArray = (int*)malloc(2 * sizeof(int));
     returnArray[0] = correctCount;
     returnArray[1] = misplacedCount;
 
     guessCount++;
 
-    printf("%s\t|\t%dA %dB\n", guess, returnArray[0], returnArray[1]);
+    printf("%d.\t%s\t|\t%dA %dB\n", guessCount, guess, returnArray[0], returnArray[1]);
 
     return returnArray;
 }
@@ -107,12 +119,13 @@ char* solve(char* guess)
     int lastCorrect;
     int lastMisplaced;
     char* lastguess;
+    int swapped = 0;
     
     //index of guess that is current changing
     digit = 3;
     
     //List of possible digits
-    int numbers[9];
+    int numbers[10];
     int  numbersIndex= 4;
     int startingIndex = 4;
     for (i=0; i<10; i++)
@@ -123,23 +136,37 @@ char* solve(char* guess)
     while (result[0] != 4)
     {
         if (numbersIndex >= 10)
-        {
-            //numbersIndex = startingIndex-1;
             numbersIndex = 0;
-        }
+        printf("carpetbomb1 %d %d %c\n", result[0], result[1], guess[0]); 
 
         lastguess = strdup(guess);
+        printf("=====================lastguess = %s\n", guess);
         lastCorrect = result[0];
         lastMisplaced = result[1];
-        
+
+        if (digit < 0)
+            digit = 3;
+                
         //Skip digits that have been marked as read
         do
         {
+            if (numbersIndex==10)
+                numbersIndex = 0;
+
+            int oldIndex;
+            if ((oldIndex = indexOfNumber(guess, numbers[numbersIndex%10])) > -1)
+            {
+
+                //if ((numbers[(int)guess[oldIndex]-'0'+1]) != -1)
+                //{
+                guess[oldIndex] = lastguess[digit];
+                swapped = 1;
+                //}
+            }
             guess[digit] = numbers[numbersIndex%10] + '0';
             numbersIndex++;
-
         }
-        while (numbers[numbersIndex-1] == -1);
+        while ((numbers[numbersIndex-1] == -1) || (strcmp(guess, lastguess) == 0));
      
         result = checkGuess(guess);
         
@@ -147,17 +174,21 @@ char* solve(char* guess)
         {
             //Mark as read
             numbers[numbersIndex-1] = -1;
+            printf("Marked %d as null\n", numbersIndex);
             
             digit--;
             startingIndex--;
             numbersIndex = startingIndex;
         }
-        else if ((lastCorrect-result[0])==1)      //If we replaced a correct digit)
+        else if ((lastCorrect-result[0])==1)      //If we replaced a correct digit
         {
             int tmpDigit;
 
             //reset guess
+            printf("=====================lastguess = %s\n", lastguess);
+            //strcpy(guess, lastguess);
             guess = lastguess;
+            printf("=====================guess = %s\n", guess);
             result[0] = lastCorrect;
             result[1] = lastMisplaced;
 
@@ -166,7 +197,7 @@ char* solve(char* guess)
             numbers[tmpDigit-1] = -1;
             numbersIndex = startingIndex;
             startingIndex--;
-            
+
             digit--;
 
         }
@@ -178,11 +209,39 @@ char* solve(char* guess)
             startingIndex = tmpDigit-1;
             
         }
-        else if (result[1] == 0)
-        {
-            numbers[numbersIndex-1] = -1;
-        }
     }
     
     return guess;
+}
+
+int indexOfNumber(char* str, int num)
+{
+    int index;
+
+    if (str==NULL || num < 0)
+        return -1;
+
+    index = 0;
+
+    while (*str!='\0')
+    {
+        if (str[0] == (num + '0'))
+            return index;
+        str++;
+        index++;
+    }
+
+    return -1;
+}
+
+int findNegativeOne(int list[], int length)
+{
+    int i;
+
+    for (i=0; i<length; i++)
+    {
+        if (list[i] == -1)
+            return i;
+    }
+    return -1;
 }
